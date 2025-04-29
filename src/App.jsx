f1dcf639ce05e41ec81aa80b40a4daaa0a39a3ef
SHON-JAGTAP <jagtapshon10@gmail.com>
2025-04-29 23:02:04 +0530

    shon updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-   
+   <div><h1>hi i am shon</h1></div>
       <div className='text-red-500'>Hello world</div>
       <button className="btn btn-accent btn-outline">Three</button>
     </>
