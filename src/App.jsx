99097c0808ec8fb14712a0b585dda188a2078091
Lokesh Pawar <lokeshpawar921@gmail.com>
2025-04-29 22:23:08 +0530

    checking merege

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+   
+      <div className='text-red-500'>Hello world</div>
+      <button className="btn btn-accent btn-outline">Three</button>
+    </>
+  )
+}
+
+export default App
